560ad31932dc221a4a64a38cea9ddbbbf7f45aed
jamie <jamiejud@gmail.com>
2019-10-11 13:21:56 +0100

    oct11

--- obmo/settings.py
+++ obmo/settings.py
@@ -13,8 +13,8 @@ SECRET_KEY = '77!1##-%^a2yfmd^s%3uyva=85!myi9ri6is$sh-i1pd0o(5)k'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False
-DEBUG = True
+DEBUG = False
+#DEBUG = True
 
 #ALLOWED_HOSTS = ['127.0.0.1','localhost']
 ALLOWED_HOSTS = ['www.objectivemoney.org','objectivemoney.org','45.55.48.199','localhost']
