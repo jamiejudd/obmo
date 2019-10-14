8867182e980ccf11234a572162d2d4b2a44adc88
jamie <jamiejud@gmail.com>
2019-10-14 23:46:21 +0100

    oct14

--- obmo/asgi.py
+++ obmo/asgi.py
@@ -1,19 +1,8 @@
-# import os
-# import channels_redis
-
-# from channels.asgi import get_channel_layer
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "obmo.settings")
-
-# channel_layer = get_channel_layer()
-
-
 
 import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatbox.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "obmo.settings")
 django.setup()
-
 application = get_default_application()
