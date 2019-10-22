abb17d102b33f7698dc001c753181e8ec21a92d3
jamie <jamiejud@gmail.com>
2019-10-22 11:12:05 +0100

    oct22

--- core/constants.py
+++ core/constants.py
@@ -1,7 +1,7 @@
 #Registered Account
-MIN_BALANCE = 0
-UBI_RATE = 1000  #qty per day
-UBI_AMOUNT = 1  #min size of installment
+MIN_BALANCE = 1 # aprox 18000
+UBI_RATE = 1000000  #qty per day(100)
+UBI_AMOUNT = 100  #min size of installment
 
 #Committments/Revelations
 TIMEDELTA_1_HOURS = 20  #in hours
@@ -13,20 +13,20 @@ LINK_WEIGHTING_PARAMETER = '1.3'  #as a string to be fed to Decimal
 
 #SettleMarkets
 MARKET_SETTLEMENT_TIME = 38 #in hours
-BET_BAD = 1
-BET_GOOD = 2
+BET_BAD = 500    # x
+BET_GOOD = 5000  # 10x
 
 #CreateChallenge
-CHALLENGER_BET = 5
-CHALLENGER_REWARD = 6
+CHALLENGER_BET = 2000
+CHALLENGER_REWARD = 2000
 
 #CreateChallengeLinks
 NUM_CHALLENGE_LINKS = 5
 CHALLENGE_LINK_WEIGHTING_PARAMETER = '2'
 
 #SettleChallenge
-CHALLENGE_SETTLEMENT_TIME = 39
-CHALLENGE_BET_BAD = 3
-CHALLENGE_BET_GOOD = 4
-CHALLENGE_BET_WHO = 7
+CHALLENGE_SETTLEMENT_TIME = 39 #in hours
+CHALLENGE_BET_BAD = 500
+CHALLENGE_BET_GOOD = 500
+CHALLENGE_BET_WHO = 500
 
